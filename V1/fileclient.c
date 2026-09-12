2c0a6543979b1edd2d07b62ca619f31bf2087d2a
findingnothing85 <findingnothing85@gmail.com>
2026-09-12 17:46:36 +0900

    Create simple fileclient.c

---
+++ V1/fileclient.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netdb.h>
+
+int main() {
+    struct addrinfo hints = {0};
+    struct addrinfo *result;
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    getaddrinfo(
+        "your-ip-address",
+        "53665",
+        &hints,
+        &result
+    );
+
+    int sockfd = socket(
+        result->ai_family,
+        result->ai_socktype,
+        result->ai_protocol
+    );
+
+    connect(
+        sockfd,
+        result->ai_addr,
+        result->ai_addrlen
+    );
+
+    char buffer[1024];
+
+    size_t n = recv(
+        sockfd,
+        buffer,
+        sizeof(buffer),
+        0
+    );
+
+    FILE *file = fopen("your-file-name", "w");
+
+    fwrite(
+        buffer,
+        1,
+        n,
+        file
+    );
+
+    fclose(file);
+    freeaddrinfo(result);
+    close(sockfd);
+
+    return 0;
+}
